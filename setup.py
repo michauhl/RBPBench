f90b50f095cdd1b1947a9a5226c2bb227a39423f
Michael Uhl <uhl.michael@gmx.de>
2023-12-22 20:20:41 +0100

    mo

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ from setuptools import setup
 
 setup(
     name='rbpbench',
-    version='0.8.1',
+    version='0.9',
     description='Evaluate CLIP-seq and other genomic region data using a comprehensive collection of known RBP binding motifs',
     long_description=open('README.md').read(),
     author='Michael Uhl',
