09d5a60ff240f3f72bcf3aff24b823d7956bbb98
Michael Uhl <uhl.michael@gmx.de>
2024-09-03 21:57:33 +0200

    mo

--- setup.py
+++ setup.py
@@ -7,11 +7,9 @@ from setuptools import setup
 
 setup(
     name='rbpbench',
-    version='0.9.1',
+    version='1.0',
     description='Evaluate CLIP-seq and other genomic region data using a comprehensive collection of known RBP binding motifs',
     long_description=open('README.md').read(),
-    author='Michael Uhl',
-    author_email='uhlm@informatik.uni-freiburg.de',
     url='https://github.com/michauhl/RBPBench',
     license='MIT',
     scripts=['bin/rbpbench', 'bin/batch_table_wrapper_rbpbench.py', 'bin/gtf_extract_gene_region_bed.py', 'bin/gtf_get_mpt_nt_freqs.py', 'bin/gtf_get_mpt_with_introns_nt_freqs.py', 'bin/gtf_get_gene_region_nt_freqs.py', 'bin/gtf_extract_exon_intron_region_bed.py', 'bin/bed_shift_regions.py'],
